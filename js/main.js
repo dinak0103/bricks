e369c8656702beda630fa0101662e37389e532de
Dziyana Kantsevich <53045550+dinak0103@users.noreply.github.com>
2022-12-07 18:11:12 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,67 @@
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader() {
+    const header = document.getElementById('header')
+    if(this.scrollY >= 50) header.classList.add('scroll-header');
+}
+window.addEventListener('scroll', scrollHeader)
+
+/*=============== SWIPER POPULAR ===============*/
+var swiperPopular = new Swiper(".popular__container", {
+    spaceBetween: 32,
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    loop: true,
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+/*=============== VALUE ACCORDION ===============*/
+const accordionItems = document.querySelectorAll('.value__accordion-item')
+
+accordionItems.forEach((item) => {
+    const accordionHeader = item.querySelector('.value__accordion-header')
+
+    accordionHeader.addEventListener('click', () => {
+        const openItem = document.querySelector('.accordion-open')
+
+        toggleItem(item)
+
+        if(openItem && openItem!== item) {
+            toggleItem(openItem)
+        }
+    })
+})
+
+const toggleItem = (item) => {
+    const accordionContent = item.querySelector('.value__accordion-content')
+
+     if(item.classList.contains('accordion-open')){
+        accordionContent.removeAttribute('style')
+        item.classList.remove('accordion-open')
+     } else {
+        accordionContent.style.height = accordionContent.scrollHeight + 'px'
+        item.classList.add('accordion-open')
+     }
+}
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    reset: true
+})
+
+sr.reveal('.home__title, .popular__container, .footer__container')
+sr.reveal('.home__description, .footer__info', {delay: 500})
+sr.reveal('.home__button', {delay: 600})
+sr.reveal('.home__value', {delay: 700})
+sr.reveal('.home__images', {delay: 800, origin: 'bottom'})
+sr.reveal('.logos__img', {interval: 100})
+sr.reveal('.value__images, .contact__content', {origin: 'left'})
+sr.reveal('.value__content, .contact__images', {origin: 'right'})
